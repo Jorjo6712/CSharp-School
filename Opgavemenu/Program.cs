a865ef2a41bda0af34458995202d1929798edd44
JorjoWasTaken <kiro.nicolov@gmail.com>
2023-05-30 13:30:30 +0200

    Opgave 1 - Done

--- Opgavemenu/Program.cs
+++ Opgavemenu/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Security.Cryptography;
+using System.Net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,30 +24,96 @@ namespace Opgavemenu{
                 Console.WriteLine("║ Opg .5   JSON string editor og syntaks checker   ║");
                 Console.WriteLine("║ Opg .6   Email checker - med loop, If, regler    ║");
                 Console.WriteLine("║ Opg .7   Email checker - regulær udtryk          ║");
+                Console.WriteLine("║ Tast et tal 1 - 7 eller q for at afslutte.       ║");
                 Console.WriteLine("╚══════════════════════════════════════════════════╝");
                 cki = Console.ReadKey();
                 
                 switch (cki.Key) {
                     case ConsoleKey.D1:
+                        retry:
                         Console.Clear();
-                        Console.WriteLine("test");
+                        
+                        Console.WriteLine("Vælge første tal: ");
+                        
+                        string input1 = Console.ReadLine();
+                        
+                        Console.WriteLine("Vælge anden tal: ");
+
+                        string input2 = Console.ReadLine();
+                        
+                        double parsedTal1;
+                        double parsedTal2;
+                                                             
+                        if (!double.TryParse(input1, out parsedTal1))
+                        {
+                            Console.WriteLine("Brug rigtig format: 10 (ikke '1 0' osv.)");
+                            Thread.Sleep(5000);
+                            goto retry;
+                        }
+
+                        if (!double.TryParse(input2, out parsedTal2))
+                        {
+                            Console.WriteLine("Brug rigtig format: 10 (ikke '1 0' osv.)");
+                            Thread.Sleep(5000);
+                            goto retry;
+                        }
+                    
+                        double resultat = parsedTal1 * parsedTal2 / (parsedTal1 - parsedTal2) + parsedTal2;
+                        
+                        if (parsedTal1 == parsedTal2)
+                        {
+                            Console.WriteLine("Divison by 0 - error");
+                            Console.WriteLine("Tryk på Q for at gå tilbage til opgavemenu.");
+
+                            ConsoleKeyInfo exit = Console.ReadKey();
+
+                            if(exit.Key == ConsoleKey.Q)
+                            {
+                                Console.Clear();
+                                Thread.Sleep(1000);
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            Console.Clear();
+
+                            Console.WriteLine("Resultat: " + resultat);
+
+                            Console.WriteLine("Tryk på Q for at gå tilbage til opgavemenu.");
+
+                            ConsoleKeyInfo exit = Console.ReadKey();
+
+                            if(exit.Key == ConsoleKey.Q)
+                            {
+                                Console.Clear();
+                                Thread.Sleep(1000);
+                                continue;
+                            }
+                        }
+
                         break;
+                    
                     case ConsoleKey.D2:
                         Console.Clear();
                         Console.WriteLine("test 2");
                         break;
+                    
                     case ConsoleKey.D3:
                         Console.Clear();
                         Console.WriteLine("test 3");
                         break;
+                    
                     case ConsoleKey.D4:
                         Console.Clear();
                         Console.WriteLine("test 4");
                         break;
+                    
                     case ConsoleKey.D5:
                         Console.Clear();
                         Console.WriteLine("test 5");
                         break;
+                    
                     case ConsoleKey.D6:
                         Console.Clear();
                         Console.WriteLine("test 6");
@@ -54,10 +122,12 @@ namespace Opgavemenu{
                         Console.Clear();
                         Console.WriteLine("test 7");
                         break;
+                    
                     case ConsoleKey.Q:
                         Console.Clear();
                         Environment.Exit(0);
                         break;    
+                    
                     default:
                         try
                         { 
