e6b95d005ae08710dd6244acfc6b10144211e558
JorjoWasTaken <93548609+JorjoWasTaken@users.noreply.github.com>
2023-08-10 15:54:28 +0200

    Program.cs Changed

---
+++ StarsAndStripes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StarsAndStripes
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            for (int i = 0; i < 12; i++) //
+            {
+
+                if (i == 0 || i == 10)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue; // changes color to blue
+                    Console.Write("                         "); // then writes empty space
+
+                    if (i == 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("                     ");
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.Write(" *   *   *   *   *   *   ");
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("                     ");
+                    }
+                }
+
+            }
+        }
+    }
+}
