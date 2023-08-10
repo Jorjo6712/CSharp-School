ffb5a5fcd060f3798f13c91aaab845972b82d7ef
Jorjo6712 <93548609+Jorjo6712@users.noreply.github.com>
2023-08-10 10:29:16 +0200

    Opgave A - Færdig

---
+++ opgaveA/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace opgaveA
+{
+    class Program
+    {
+        static void Main()
+        {
+            int length = 16;
+            double width = 5.1;
+
+            Console.WriteLine(width + length);
+
+            //Resultat: 21,1
+        }
+    }
+}
