27af225e7873965dda74614a51010e6175ba5c2d
Jorjo6712 <93548609+Jorjo6712@users.noreply.github.com>
2023-08-10 10:31:48 +0200

    Opgave C og B - Færdig

---
+++ opgaveB/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace opgaveB
+{
+    class Program
+    {
+        static void Main()
+        {
+            int area = width*height;
+            //Det virker ikke fordi width og height variablerne er ikke initializet 
+        }
+    }
+}
---
+++ opgaveC/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace opgaveC
+{
+    class Program
+    {
+        static void Main()
+        {
+            double d = width*length;
+            //Det virker ikke fordi width og length variablerne er ikke initializet
+        }
+    }
+}
