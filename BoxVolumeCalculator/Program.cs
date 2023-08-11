6c56e2e9f46e4254ca25c391a546a08e7b028188
Jorjo6712 <93548609+Jorjo6712@users.noreply.github.com>
2023-08-11 08:10:29 +0200

    Spelling error fix

--- BoxVolumeCalculator/Program.cs
+++ BoxVolumeCalculator/Program.cs
@@ -1,33 +1,33 @@
-﻿namespace Rumfanget 
+﻿namespace Rumfanget
 {
     class Program
     {
         static void Main()
         {
             Console.Clear();
-            
+
             Console.WriteLine("Box Volume Calculator \n");
-            
-            Console.WriteLine("Write your height: ");
-            
+
+            Console.WriteLine("Write the height: ");
+
             int height = Convert.ToInt32(Console.ReadLine()); // Reads height from user input
-            
-            Console.WriteLine("Write your width: ");
+
+            Console.WriteLine("Write the width: ");
 
             int width = Convert.ToInt32(Console.ReadLine()); // Reads width from user input
 
-            Console.WriteLine("Write your length: ");
+            Console.WriteLine("Write the length: ");
 
             int length = Convert.ToInt32(Console.ReadLine()); // Reads length from user input
 
             int result = height * width * length; // Multiplies height, width and length and stores in result
 
-            Console.WriteLine($"\nResulat: {result} \n"); // Prints result to console
+            Console.WriteLine($"\nResult: {result} \n"); // Prints result to console
 
             Console.WriteLine("Press any key to exit...");
 
             Console.ReadKey();
-            
+
         }
     }
 }
