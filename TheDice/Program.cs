e3a16004d3d8b02d4bdaa22bac03da897e414bc3
Jorjo6712 <93548609+Jorjo6712@users.noreply.github.com>
2023-08-11 12:42:56 +0200

    the dice

---
+++ TheDice/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace kontrolstrukturtest
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine("Press the enter key to roll dice!\n");
+
+                ConsoleKeyInfo cki = Console.ReadKey();
+
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    Random random = new Random();
+
+                    int dice = random.Next(1, 7);
+
+                    Console.Clear();
+
+                    Console.WriteLine($"You rolled a {dice}\n");
+                }
+                else if (cki.Key != ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    continue;
+                }
+            }
+        }
+    }
+}
