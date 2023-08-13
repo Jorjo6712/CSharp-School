14393047119315b15f5839850d5b4f92b8ec362a
Jorjo6712 <93548609+Jorjo6712@users.noreply.github.com>
2023-08-13 11:31:01 +0200

    commit

---
+++ BMI-Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BMICalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("BMI Calculator");
+
+            Console.WriteLine("Write your heigth: ");
+
+            double? height = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Write your weight: ");
+
+            double? weight = Convert.ToDouble(Console.ReadLine());
+
+
+        }
+    }
+}
