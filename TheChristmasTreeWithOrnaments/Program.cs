56edc43f9f2f372d77ee9158191d1be54168b730
JorjoWasTaken <kiro.nicolov@gmail.com>
2023-08-14 20:09:26 +0200

    I couldn't figure it out

---
+++ TheChristmasTreeWithOrnaments/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TheChristmasTreeWithOrnaments
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.Write("*");
+
+                if (i == 0 || i == 2 || i == 4 || i == 6)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine();
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
