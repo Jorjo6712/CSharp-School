2a802bc1f8df84e267330d2997f47200d9381943
JorjoWasTaken <kiro.nicolov@gmail.com>
2023-08-14 22:32:40 +0200

    not done

---
+++ Nim/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Nim
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Welcome to NIM! \n");
+
+            while (true)
+            {
+                try
+                {
+                    int matches = 7;
+
+                    bool playerTurn;
+
+                    bool gameOver;
+
+                    Console.WriteLine("Your turn! Choose between 1-3 matches.");
+
+                    int playerInput = Convert.ToInt32(Console.ReadLine());
+
+
+                    if (matches == 0)
+                    {
+                        matches += 1;
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Fejl: " + ex.Message);
+                }
+            }
+        }
+    }
+}
