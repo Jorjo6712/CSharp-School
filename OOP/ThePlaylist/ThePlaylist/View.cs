c1324a1c07d3bc520d717fc093d3e00875f5bc7f
Jorjo6712 <93548609+Jorjo6712@users.noreply.github.com>
2023-09-15 21:37:06 +0200

    ThePlaylist done

---
+++ OOP/ThePlaylist/ThePlaylist/View.cs
@@ -0,0 +1,15 @@
+﻿namespace ThePlaylist
+{
+    internal class View
+    {
+        internal void Text(string text) 
+        {
+             Console.WriteLine(text);
+        }
+
+        internal void ReadLine()
+        {
+            Console.ReadLine();
+        }
+    }
+}
